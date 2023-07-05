2ac688d25671480aca3980d3ba111f00f5ec9547
Andrew W. Steiner <awsteiner@mykolab.com>
2023-07-05 14:35:31 -0400

    Adding omp_test files.

---
+++ omp_test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+int main(void) {
+
+  vector<size_t> arr;
+  
+#pragma omp parallel
+  {
+#pragma omp for
+    for(size_t i=0;i<100;i++) {
+      arr.push_back(i);
+    }
+  }
+  
+  for(size_t i=0;i<100;i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+               
---
+++ omp_test2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+int main(void) {
+
+  vector<size_t> arr(100);
+  
+#pragma omp parallel
+  {
+#pragma omp for
+    for(size_t i=0;i<100;i++) {
+      arr[i]=i;
+    }
+  }
+  
+  for(size_t i=0;i<100;i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+               
